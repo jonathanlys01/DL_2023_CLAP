8f7fef338ad32fe7d0474a38f4c65f3e7d144dec
jonathanlys01 <jonathanlystahiti@gmail.com>
2023-11-09 18:06:51 +0100

    main can process esc50 ds

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # DL_2023_CLAP
 Experiments on the CLAP model for the DL course at IMTA (2023-2024)
+
+## A few experiments results
+
+### Last audio processed
+Image of the last audio processed by the model.
+
+<img src="last_audio.png" width="100%" alt="last audio">
+
+### Confusion matrix of the model over the ESC-50 dataset
+
+<img src="confusion_matrix.png" width="100%" alt="Confusion matrix">
--- main.py
+++ main.py
@@ -3,52 +3,114 @@ import argparse
 from dataset import ESC_50
 import numpy as np
 import matplotlib.pyplot as plt
-import uuid
-
+import torch
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--path_to_audio", "-a", type=str, required=True)
-parser.add_argument("--path_to_annotation", "-t", type=str, required=True)
+parser.add_argument("--path_to_audio", "-a", type=str, required=True, help="Path to audio files")
+parser.add_argument("--path_to_annotation", "-t", type=str, required=True, help="Path to annotation files")
+parser.add_argument("--plot", "-p", action=argparse.BooleanOptionalAction, default=False, help="Plot all audios")
 
 args = parser.parse_args()
 
+print()
 esc_50 = ESC_50(args.path_to_audio, args.path_to_annotation)
 
 start = time.time()
 
 from transformers import ClapModel, ClapProcessor
 
-
-import_time = time.time()
-print(f"Import time: {import_time - start:.2f}")
+print("Loading model...")
 
 model = ClapModel.from_pretrained("laion/clap-htsat-unfused").to("cuda")
 processor = ClapProcessor.from_pretrained("laion/clap-htsat-unfused")
 
+print(f"Model loaded in {time.time() - start:.2f} seconds")
 
-#ind = np.random.randint(0, len(esc_50))
-ind = 1
-filename = esc_50.audios[ind]
-label = esc_50.annotations[ind]
-
-
-audio, sr = esc_50.open_audio(filename, sr=48000)
-
-plt.plot(audio)
-plot_name = filename.split("/")[-1].split(".")[0]
-plt.title(f"Audio: {plot_name} / Label: {label}") 
-plt.savefig(f"temp/audio_{uuid.uuid4()}.png")
-
+preds = []
+limit = 200
 texts = esc_50.classes
-
 inputs_text = processor(text=texts, return_tensors="pt", padding=True)
-print(inputs_text)
 
-inputs_audio = processor(audios=audio, sampling_rate=sr, return_tensors="pt")
+for key, value in inputs_text.items():
+        inputs_text[key] = value.to("cuda")
 
-outputs = model(**inputs_text, **inputs_audio)
+with torch.inference_mode():
+    outputs_text = model.get_text_features(**inputs_text)
+
+pbar = tqdm(range(min(limit, len(esc_50.audios))))
+
+for ind in pbar:
+    filename = esc_50.audios[ind]
+    label = esc_50.annotations[ind]
+
+    audio, sr = esc_50.open_audio(filename, sr=48000)
+
+    plt.plot(audio)
+    plot_name = filename.split("/")[-1].split(".")[0]
+    plt.title(f"Audio: {plot_name} / Label: {label}")
+    if args.plot:
+        plt.savefig(f"temp/audio_{plot_name}.png")
+    plt.savefig(f"last_audio.png")
+    plt.close()
+
+    inputs_audio = processor(audios=audio, sampling_rate=sr, return_tensors="pt")
+
+    for key, value in inputs_audio.items():
+        inputs_audio[key] = value.to("cuda")
+
+    with torch.inference_mode():
+        outputs_audio = model.get_audio_features(**inputs_audio)
+
+    cosine_sim = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
+
+    sim = cosine_sim(outputs_text, outputs_audio)
+
+    pred_index = torch.argmax(sim).item()
+    
+    preds.append(esc_50.classes[pred_index])
+    
+    acc = 100 * np.mean(np.array(preds) == np.array(esc_50.annotations[:len(preds)]))
+    
+    pbar.set_description(f"Accuracy: {acc:.2f}%")
+    
+
+print(f"Accuracy: {acc:.2f}")
+
+classes_pred = np.unique(preds)
+classes_true = np.unique(esc_50.annotations)
+
+all_classes = np.unique(np.concatenate((classes_pred, classes_true)))
+
+confusion_matrix = np.zeros((len(all_classes), len(all_classes)))
+
+for i in range(len(preds)):
+    row = np.where(all_classes == preds[i])[0][0]
+    col = np.where(all_classes == esc_50.annotations[i])[0][0]
+    confusion_matrix[row, col] += 1
+    
+plt.figure(figsize=(10, 10))
+plt.imshow(confusion_matrix, cmap="Blues", interpolation="nearest")
+plt.xticks(np.arange(len(all_classes)), all_classes, rotation=90)
+plt.yticks(np.arange(len(all_classes)), all_classes)
+plt.xlabel("True")
+plt.ylabel("Predicted")
+plt.colorbar()
+plt.tight_layout()
+plt.title(f"Confusion matrix (Accuracy: {acc:.2f}%)")
+
+plt.savefig("confusion_matrix.png")
+
+
 end = time.time()
-print(f"Total time: {end - start:.2f}")
+import datetime
+
+elapsed = str(datetime.timedelta(seconds=(end - start))) # convert time to hh:mm:ss.nnnnnn format
+elapsed.split(".")[0] # remove microseconds
+if elapsed.startswith("0:"):
+    elapsed = elapsed[2:] # remove leading 0:
+
+print(f"Elapsed time: {elapsed}")
 
 

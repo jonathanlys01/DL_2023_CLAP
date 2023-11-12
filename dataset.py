f2247b2f30ded1fe2262002e76259f6019f52204
jonathanlys01 <jonathanlystahiti@gmail.com>
2023-11-12 20:19:08 +0100

    add support for 3 datasets

--- dataset.py
+++ dataset.py
@@ -3,6 +3,7 @@ import os
 import librosa
 from torch.utils.data import Dataset 
 import pandas as pd
+import random as rd
 
 def check_audio(path: str):
     if any([path.endswith(ext) for ext in ['.wav', '.mp3', '.flac']]):
@@ -33,7 +34,12 @@ class ESC_50(Dataset):
 
         assert len(self.audios) == len(self.annotations), "Number of audios and annotations do not match"
 
-        print(f"Dataset loaded with {len(self.audios)} samples")
+        indexes = [i for i in range(len(self.audios))]
+        rd.shuffle(indexes)
+        self.audios = [self.audios[i] for i in indexes]
+        self.annotations = [self.annotations[i] for i in indexes]
+
+        print(f"Dataset loaded with {len(self.audios)} samples (ESC-50)")
     
     def __len__(self):
         return len(self.audios)
@@ -47,14 +53,120 @@ class ESC_50(Dataset):
         return y, sr
     
 
+class FMA(Dataset):
+    def __init__(self, path_to_audio, path_to_annotation):
+        # nested paths
+        list_folders = [os.path.join(path_to_audio, f)\
+            for f in os.listdir(path_to_audio)\
+            if os.path.isdir(os.path.join(path_to_audio, f))]
+        # [path_to_audio/000, path_to_audio/001, ...]
+        # there's a README.md file in the root of the dataset, so we need to remove it
+        
+        audios = []
+        for folder in list_folders:
+            audios += [os.path.join(folder, audio) for audio in os.listdir(folder)]
+        # [path_to_audio/000/000002.mp3, path_to_audio/000/000005.mp3, ...]
+        
+        self.audios = [audio for audio in audios if check_audio(audio)]
+        rd.shuffle(self.audios) # in-place shuffle
+        
+        df_fma = pd.read_csv(path_to_annotation, header=[0, 1], index_col=0)
+        indexes_small = df_fma.index[df_fma["set", "subset"] == "small"]
+        df_fma_small = df_fma.loc[indexes_small]
+        del df_fma
+        annotations = {index:genre for index, genre in zip(df_fma_small.index, 
+                                                           df_fma_small["track", "genre_top"])}
+        # {track_id: genre, ...}
+        
+        temp_audio_ids = [int(audio.split("/")[-1].split(".")[0]) for audio in self.audios] # [track_id, ...]
+        self.annotations = [annotations[audio_id] for audio_id in temp_audio_ids] 
+        # reordering annotations to match audios
+        del annotations
+        del df_fma_small
+        
+        self.classes = sorted(list(set(self.annotations)))
+        
+        assert len(self.audios) == len(self.annotations), "Number of audios and annotations do not match"
+        
+        print(f"Dataset loaded with {len(self.audios)} samples (FMA)")
+    
+    def __len__(self):
+        return len(self.audios)
+    
+    def __getitem__(self, idx):
+        return {"text": self.annotations[idx], "audio": self.audios[idx]}
+    
+    def open_audio(self,path, sr=None):
+        y, sr = librosa.load(path, sr=sr)
+        return y, sr
+
+
+class UrbanSound8k(Dataset):
+    def __init__(self, path_to_audio, path_to_annotation):
+        df = pd.read_csv(path_to_annotation)
+        
+        self.audios = [os.path.join((path_to_audio), 
+                       f"fold{x['fold']}", 
+                       x["slice_file_name"]) for _, x in df.iterrows()]
+        assert all([os.path.exists(audio) for audio in self.audios]), "Not all audios exist"
+        
+        self.audios = [audio for audio in self.audios if check_audio(audio)]
+        
+        self.annotations = df["class"].values
+        
+        assert len(self.audios) == len(self.annotations), "Number of audios and annotations do not match"
+        
+        indexes = [i for i in range(len(self.audios))]
+        rd.shuffle(indexes)
+        self.audios = [self.audios[i] for i in indexes]
+        self.annotations = [self.annotations[i] for i in indexes]
+        
+        self.classes = sorted(list(set(self.annotations)))
+        print(f"Dataset loaded with {len(self.audios)} samples (UrbanSound8K)")
+    def __len__(self):
+        return len(self.audios)
+    def __getitem__(self, idx):
+        return {"text": self.annotations[idx], "audio": self.audios[idx]}
+    
+    def open_audio(self,path, sr=None):
+        y, sr = librosa.load(path, sr=sr)
+        return y, sr
+        
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-
-    parser.add_argument("--path_to_audio", "-a", type=str, required=True)
-    parser.add_argument("--path_to_annotation", "-t", type=str, required=True)
-
-    args = parser.parse_args()
-
-    dataset = ESC_50(args.path_to_audio, args.path_to_annotation)
+    help = "Dataset type, one of ESC-50, FMA, UrbanSound8K (short names: e, f, u)"
+    parser.add_argument("--dataset_type", "-d", type=str, required=True, help=help)
+    root = "downloads/"
     
-    print(dataset.classes)
+    args = parser.parse_args()
+    ds_type = args.dataset_type
+    
+    if ds_type.lower() in ["esc-50", "esc50", "esc","e"]:
+        ds_type = "ESC-50"
+    elif ds_type.lower() in ["fma", "freemusicarchive","f"]:
+        ds_type = "FMA"
+    elif ds_type.lower() in ["urbansound8k", "urbansound", "urban","u"]:
+        ds_type = "UrbanSound8K"
+    
+    if ds_type == "ESC-50":
+        print("Loading ESC-50 dataset")
+        path_to_audio = os.path.join(root, "ESC-50-master/audio/")
+        path_to_annotation = os.path.join(root, "ESC-50-master/meta/esc50.csv")
+        dataset = ESC_50(path_to_audio, path_to_annotation)
+        print("classes",dataset.classes)
+        
+    elif ds_type == "FMA":
+        print("Loading FMA dataset")
+        path_to_audio = os.path.join(root, "fma_small/")
+        path_to_annotation = os.path.join(root, "fma_metadata/tracks.csv")
+        dataset = FMA(path_to_audio, path_to_annotation)
+        print("classes",dataset.classes)
+    
+    elif ds_type == "UrbanSound8K":  
+        print("Loading UrbanSound8K dataset")
+        path_to_audio = os.path.join(root, "UrbanSound8K/audio/")
+        path_to_annotation = os.path.join(root, "UrbanSound8K/metadata/UrbanSound8K.csv")
+        dataset = UrbanSound8k(path_to_audio, path_to_annotation)
+        print("classes",dataset.classes)
+    
--- dataset.sh
+++ dataset.sh
@@ -5,39 +5,54 @@ mkdir -p downloads
 cd downloads
 
 # Download the datasets if they are not already downloaded
-# Count the number of datasets downloaded
-$c = 0
+counter=0
 
 # ESC-50
 if [ ! -d "ESC-50-master" ]; then
+    echo "ESC-50-master not found, downloading..."
     wget https://github.com/karoldvl/ESC-50/archive/master.zip
+    echo "Unzipping..."
     unzip -q master.zip
     rm master.zip
-    $c = $c + 1
+    ((counter++))
+else
+    echo "ESC-50-master found, skipping..."
 fi
 
 # UrbanSound8K
 if [ ! -d "UrbanSound8K" ]; then
+    echo "UrbanSound8K not found, downloading..."
     wget https://zenodo.org/record/1203745/files/UrbanSound8K.tar.gz
+    echo "Unzipping..."
     tar -xzf UrbanSound8K.tar.gz
     rm UrbanSound8K.tar.gz
-    $c = $c + 1
+    ((counter++))
+else
+    echo "UrbanSound8K found, skipping..."
 fi
 
 # FMA Small
 if [ ! -d "fma_small" ]; then
+    echo "fma_small not found, downloading..."
     wget https://os.unil.cloud.switch.ch/fma/fma_small.zip
+    echo "Unzipping..."
     unzip -q fma_small.zip
     rm fma_small.zip
-    $c = $c + 1
+    ((counter++))
+else
+    echo "fma_small found, skipping..."
 fi
 
 # FMA Metadata
 if [ ! -d "fma_metadata" ]; then
+    echo "fma_metadata not found, downloading..."
     wget https://os.unil.cloud.switch.ch/fma/fma_metadata.zip
+    echo "Unzipping..."
     unzip -q fma_metadata.zip
     rm fma_metadata.zip
-    $c = $c + 1
+    ((counter++))
+else
+    echo "fma_metadata found, skipping..."
 fi
 
 echo "Downloaded $c datasets."
@@ -48,8 +63,8 @@ du -sh ./*
 echo "chmod 777 -R downloads ?"
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
-    chmod 777 -R downloads
+    chmod 777 -R ../downloads
 fi
-
+cd ..
 echo "Done !"
 
